f1d89764283db405d41bbd1f80778d71d1875415
leadgfl13 <leadgfl13@gmail.com>
2025-01-24 17:47:39 -0500

    Log JSON data, and allow a location to be put in as an argument, using a submit button

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
 import "./style.css";
 
-document.addEventListener("click", () => {
-	alert("hello");
-});
+const searchbutton = document.getElementById("submit");
+searchbutton.addEventListener("click", () => {
+	let location = document.getElementById("location");
+	let test = location.value;
 
+	console.log(test);
+	getWeather(test);
+});
 function getAPI() {}
 
 //this will be the function to get the location from somwewhere
 function getData() {
 	//key is ZVFB4SWSDYVUP2SNNA5AYMS7W
 }
-
 //ZVFB4SWSDYVUP2SNNA5AYMS7W
-async function getWeather() {
-	const response = await fetch(
-		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=VFB4SWSDYVUP2SNNA5AYMS7W`
+async function getWeather(location) {
+	const thing = await fetch(
+		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location},US?key=ZVFB4SWSDYVUP2SNNA5AYMS7W`,
+		{ mode: "cors" }
 	);
-	response.json().then(function (response) {
-		test = response.data;
-	});
-	const Cat = await response.json();
-	console.log(Cat);
+	const thingy = await thing.json();
+	let address = thingy.address;
+	let days = thingy.days;
+	console.log(thingy);
+	console.log(address);
+	console.log(days[1].tempmax);
 }
+
+//address tells us the location
 //"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=ZVFB4SWSDYVUP2SNNA5AYMS7W "
 
 //"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[auburn]/[date1]/[date2]?key=ZVFB4SWSDYVUP2SNNA5AYMS7W "
 
-getWeather();
 // Dates should be in the format yyyy-MM-dd. For example 2020-10-19
 //Only one date is needed, but you can search through the second date
 

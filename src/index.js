bde0415f082589769e5e2e56e38cc439a42af4c9
leadgfl13 <leadgfl13@gmail.com>
2025-01-25 18:47:57 -0500

    Have the first day's weather show up on right side of page

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ const states = [
 	"Wyoming",
 ];
 // sets the search button
+const right = document.getElementById("right");
 const searchbutton = document.getElementById("submit");
 //adds the states to the dropdown menu
 const drop = document.getElementById("stateDropdown");
@@ -65,22 +66,11 @@ for (const state of states) {
 	drop.appendChild(option);
 }
 
-searchbutton.addEventListener("click", (e) => {
-	e.preventDefault();
-	let location = document.getElementById("location");
-	let test = location.value;
-	let state = getState();
-	console.log(test);
-	getWeather(test, state);
-});
-function getAPI() {}
-
-//this will be the function to get the location from somwewhere
-function getData() {
-	//key is ZVFB4SWSDYVUP2SNNA5AYMS7W
-}
-//ZVFB4SWSDYVUP2SNNA5AYMS7W
+//this will be the function to call the API service and get info.
 async function getWeather(location, thestate) {
+	//key is ZVFB4SWSDYVUP2SNNA5AYMS7W (API Key)
+
+	//
 	const thing = await fetch(
 		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}, ${thestate}US?key=ZVFB4SWSDYVUP2SNNA5AYMS7W`,
 		{ mode: "cors" }
@@ -91,19 +81,41 @@ async function getWeather(location, thestate) {
 	console.log(thingy);
 	console.log(address);
 	console.log(days[1].description);
-	alert(
+	let info =
 		"today's weather is " +
-			days[1].description +
-			" with a high of " +
-			days[1].tempmax
-	);
+		days[1].description +
+		" with a high of " +
+		days[1].tempmax;
+
+	makeBox(1, 25, info);
 }
 
+//this gets the state value from dropdown menu
 function getState() {
 	let theState = drop.value;
 	return theState;
 }
 
+//Make a box that displys information
+function makeBox(numboxes, date, description) {
+	let box = document.createElement("div");
+	let desc = document.createElement("div");
+	desc.innerHTML = description;
+	const right = document.getElementById("right");
+
+	box.append(desc);
+	right.append(box);
+}
+
+//event listeners go here
+searchbutton.addEventListener("click", (e) => {
+	e.preventDefault();
+	let location = document.getElementById("location");
+	let test = location.value;
+	let state = getState();
+	console.log(test);
+	getWeather(test, state);
+});
 //address tells us the location
 //"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=ZVFB4SWSDYVUP2SNNA5AYMS7W "
 
